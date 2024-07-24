20cd6831771e6f57719fec8b49989b75ed6722ca
jesus-333 <albi25.zanca@gmail.com>
2024-07-24 19:19:12 +0200

    Work on sdtw v1

--- src/sdtw_v1.rs
+++ src/sdtw_v1.rs
@@ -1,5 +1,5 @@
 use ndarray::prelude::*;
-use numpy::ndarray::{ArrayViewD, ArrayViewMutD};
+use numpy::ndarray::{ArrayViewD};
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
@@ -8,14 +8,35 @@ pub fn compute_1d(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>) -> f32 {
 
     print_type_of(&x);
 
-    // let distance_matrix = Array::zeros([x.len(), y.len()]);
-    let distance_matrix = Array::<f32, _>::zeros((x.len(), y.len()).f());
+    // Create distance matrix. Even if I do this initial check for now I conside x and y of the
+    // same dimension.
+    // TODO implement version with x and y of different length
+    let distance_matrix = if x.len() >= y.len() {
+        Array::<f32, _>::zeros((x.len(), y.len()).f());
+    } else {
+        Array::<f32, _>::zeros((y.len(), x.len()).f());
+    };
 
-    let n_diagonals = x.len() + y.len();
+    let n_diagonals = x.len() + y.len() - 1;
+    println!("{}, {}, {}", n_diagonals, x.len(), y.len());
+    
+    let mut n_elements_diagonal = 1;
+    for n_1 in 1..n_diagonals{ // Iterate through the diagonals
+        
+        // Compute the number of element in the diagonals
+        // TODO modify when add arrays of different length
+        if n_1 <= n_diagonals / 2 { // Upper left of the distance matrix
+            n_elements_diagonal += 1;
+        } else { // Lower right of the distance matrix
+            n_elements_diagonal -= 1;
+        }
+        
+        // Indices
+        let idx = Array::<i32, _>::range(0., n_elements_diagonal as f32, 1.);
+        for n_2 in 0..n_elements_diagonal {
 
-    // print!("{:?}\n", distance_matrix.dim());
-    // print!("{:?}\n", distance_matrix.shape());
-    // print!("{}\n", distance_matrix.shape()[0]);
+        }
+    }
 
     return result
 }
