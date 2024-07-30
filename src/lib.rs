5c414805b583480d3c4e6df90f9efd2d3ae761cb
jesus-333 <albi25.zanca@gmail.com>
2024-07-30 19:33:32 +0200

    Convert float from f32 to f64
    With f32 there where not enough numerical precision with some
    computation and the final results was infinite.

--- src/lib.rs
+++ src/lib.rs
@@ -18,14 +18,14 @@ fn soft_dtw_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
 // Function in the python module
 
 // #[pyfunction] // NOT WORK
-fn get_distance_matrix(x : PyReadonlyArrayDyn<f32>, y : PyReadonlyArrayDyn<f32>, gamma : Option<f32>) -> PyResult<Array2<f32>> {
+fn get_distance_matrix(x : PyReadonlyArrayDyn<f64>, y : PyReadonlyArrayDyn<f64>, gamma : Option<f64>) -> PyResult<Array2<f64>> {
     // sdtw_v1::print_type_of(&x);
 
     Ok(sdtw_v1::compute_distance_matrix(x.as_array(), y.as_array(), gamma.unwrap_or(1.)))
 }
 
 #[pyfunction]
-fn compute_sdtw_1d(x : PyReadonlyArrayDyn<f32>, y : PyReadonlyArrayDyn<f32>, gamma : Option<f32>) -> PyResult<f32> {
+fn compute_sdtw_1d(x : PyReadonlyArrayDyn<f64>, y : PyReadonlyArrayDyn<f64>, gamma : Option<f64>) -> PyResult<f64> {
     // sdtw_v1::print_type_of(&x);
     Ok(sdtw_v1::compute_sdtw(x.as_array(), y.as_array(), gamma.unwrap_or(1.)))
 }
--- src/sdtw_v1.rs
+++ src/sdtw_v1.rs
@@ -4,7 +4,7 @@ use numpy::ndarray::{ArrayViewD};
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-pub fn compute_sdtw(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>, gamma : f32) -> f32 {
+pub fn compute_sdtw(x : ArrayViewD<'_, f64>, y : ArrayViewD<'_, f64>, gamma : f64) -> f64 {
     let x_len = x.len();
     let y_len = y.len();
 
@@ -13,7 +13,7 @@ pub fn compute_sdtw(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>, gamma : f3
     return distance_matrix[[x_len -1, y_len -1]];
 }
 
-pub fn compute_distance_matrix(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>, gamma : f32) -> Array2<f32> {
+pub fn compute_distance_matrix(x : ArrayViewD<'_, f64>, y : ArrayViewD<'_, f64>, gamma : f64) -> Array2<f64> {
     
     let gamma_for_computation = if gamma <= 0. {
         println!("gamma must be non negative. Current value is {}. Set the value to 1", gamma);
@@ -28,18 +28,15 @@ pub fn compute_distance_matrix(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>,
     // same dimension.
     // TODO implement version with x and y of different length
     let mut distance_matrix = if x.len() >= y.len() {
-        Array::<f32, _>::zeros((x.len(), y.len()).f())
+        Array::<f64, _>::zeros((x.len(), y.len()).f())
     } else {
-        Array::<f32, _>::zeros((y.len(), x.len()).f())
+        Array::<f64, _>::zeros((y.len(), x.len()).f())
     };
     
     // Compute element in position (0, 0)
     distance_matrix[[0, 0]] = l_norm(x[0], y[0], 2);
 
-    println!("{:?}", distance_matrix);
-
     let n_diagonals = x.len() + y.len() - 1;
-    println!("{}, {}, {}", n_diagonals, x.len(), y.len());
     
     // The value of the soft-dtw are compute in diagonals, starting from the upper left corner and
     // proceding toward the lower right corner.
@@ -51,7 +48,6 @@ pub fn compute_distance_matrix(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>,
     // section of the matrix)
     let mut shift = 0;
     
-    println!("n_diagonals = {}, n_diagonals / 2 = {}", n_diagonals, n_diagonals / 2);
     // Compute soft-dtw for each element of the matrix
     for n_1 in 1..n_diagonals{ // Iterate through the diagonals
         // Compute the number of element in the diagonals
@@ -76,7 +72,7 @@ pub fn compute_distance_matrix(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>,
         // println!("n_1 = {} ({}), n_elements_diagonal = {}, upper_left_section = {}, shift = {}", n_1, n_1 + 1, n_elements_diagonal, computation_in_the_upper_left_section, shift);
 
         // Indices
-        // let idx_array = Array::<f32, _>::range(0., n_elements_diagonal as f32, 1.);
+        // let idx_array = Array::<f64, _>::range(0., n_elements_diagonal as f64, 1.);
         // idx 1 is used for the row of the matrix
         // idx 2 is used for the column of the matrix
         for n_2 in 0..n_elements_diagonal {
@@ -88,28 +84,31 @@ pub fn compute_distance_matrix(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>,
             // println!("\tidx_1 = {}, idx_2 = {}", idx_1, idx_2);
 
             // Get the value above the one I want to compute
-            let left_value = if idx_1 - 1 < 0 {
-                f32::MAX
+            let left_value = if idx_1 as i64 - 1 < 0 {
+                i16::MAX as f64
             } else {
                 distance_matrix[[idx_1 - 1, idx_2]]
             };
             
             // Get the value above the one I want to compute
-            let upper_value = if idx_2 - 1 < 0 {
-                f32::MAX
+            let upper_value = if idx_2 as i64 - 1 < 0 {
+                i16::MAX as f64
             } else {
                 distance_matrix[[idx_1, idx_2 - 1]]
             };
 
             // Get the value above and on the left (i.e. in diagonal) respect the one I want to compute
-            let upper_left_value = if idx_1 - 1 < 0 || idx_2 -1 < 0 {
-                f32::MAX
+            let upper_left_value = if idx_1 as i64 - 1 < 0 || idx_2 as i64 -1 < 0 {
+                i16::MAX as f64
             } else {
                 distance_matrix[[idx_1 - 1, idx_2 - 1]]
             };
             
             // Compute the dtw value for the current cell
-            distance_matrix[[idx_1, idx_2]] = l_norm(x[idx_1], y[idx_2], 2) + compute_soft_min_optimized(upper_value, upper_left_value, left_value, gamma);
+            let l_norm_value = l_norm(x[idx_1], y[idx_2], 2);
+            let soft_min_values = compute_soft_min_optimized(upper_value, upper_left_value, left_value, gamma);
+            distance_matrix[[idx_1, idx_2]] = l_norm_value + soft_min_values;
+
         }
     }
 
@@ -119,8 +118,8 @@ pub fn compute_distance_matrix(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>,
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 // Mathematical funtions
 
-fn l_norm(x : f32, y : f32, n : usize) -> f32 {
-    return f32::powf(num::pow(x - y, n), 1 as f32 / n as f32)
+fn l_norm(x : f64, y : f64, n : usize) -> f64 {
+    return f64::powf(num::pow(x - y, n), 1 as f64 / n as f64)
 }
 
 /// Optimized version of the soft min for the soft-dtw computation
@@ -129,13 +128,11 @@ fn l_norm(x : f32, y : f32, n : usize) -> f32 {
 /// Calling this 3 value x, y and z, and the new value soft_min then this function compute
 /// soft_min = -gamma * ln(exp(-x/gamma) + exp(-y/gamma) + exp(-z/gamma))
 /// where gamme is a non negative hyperparameter
-fn compute_soft_min_optimized(upper_value : f32, upper_left_value : f32, left_value : f32, gamma : f32) -> f32{
-    
+fn compute_soft_min_optimized(upper_value : f64, upper_left_value : f64, left_value : f64, gamma : f64) -> f64{
     return  -gamma * ((-upper_value/gamma).exp() + (-upper_left_value/gamma).exp() + (-left_value/gamma).exp()).ln()
-
 }
 
-fn compute_soft_min_general(values_array : &[f32], gamma : f32) -> f32 {
+fn compute_soft_min_general(values_array : &[f64], gamma : f64) -> f64 {
     let mut soft_min = 0.0;
     for &value in values_array.iter(){
         soft_min += (-(value / gamma)).exp();
