45ac20f69f7c35a3b042b8c0beae518f3bcd33ce
jesus-333 <albi25.zanca@gmail.com>
2024-07-30 18:06:20 +0200

    Complete function to compute distance matrix and sdtw computation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use pyo3::prelude::*;
+use ndarray::prelude::*;
 
 use numpy::{PyReadonlyArrayDyn, PyArrayDyn};
 
@@ -16,8 +17,15 @@ fn soft_dtw_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 // Function in the python module
 
+// #[pyfunction] // NOT WORK
+fn get_distance_matrix(x : PyReadonlyArrayDyn<f32>, y : PyReadonlyArrayDyn<f32>, gamma : Option<f32>) -> PyResult<Array2<f32>> {
+    // sdtw_v1::print_type_of(&x);
+
+    Ok(sdtw_v1::compute_distance_matrix(x.as_array(), y.as_array(), gamma.unwrap_or(1.)))
+}
+
 #[pyfunction]
-fn compute_sdtw_1d(x : PyReadonlyArrayDyn<f32>, y : PyReadonlyArrayDyn<f32>) -> PyResult<f32> {
-    sdtw_v1::print_type_of(&x);
-    Ok(sdtw_v1::compute_1d(x.as_array(), y.as_array()))
+fn compute_sdtw_1d(x : PyReadonlyArrayDyn<f32>, y : PyReadonlyArrayDyn<f32>, gamma : Option<f32>) -> PyResult<f32> {
+    // sdtw_v1::print_type_of(&x);
+    Ok(sdtw_v1::compute_sdtw(x.as_array(), y.as_array(), gamma.unwrap_or(1.)))
 }
--- src/sdtw_v1.rs
+++ src/sdtw_v1.rs
@@ -4,8 +4,16 @@ use numpy::ndarray::{ArrayViewD};
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-pub fn compute_1d(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>, gamma : f32) -> f32 {
-    let result = 1.;
+pub fn compute_sdtw(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>, gamma : f32) -> f32 {
+    let x_len = x.len();
+    let y_len = y.len();
+
+    let distance_matrix = compute_distance_matrix(x, y, gamma);
+
+    return distance_matrix[[x_len -1, y_len -1]];
+}
+
+pub fn compute_distance_matrix(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>, gamma : f32) -> Array2<f32> {
     
     let gamma_for_computation = if gamma <= 0. {
         println!("gamma must be non negative. Current value is {}. Set the value to 1", gamma);
@@ -19,11 +27,15 @@ pub fn compute_1d(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>, gamma : f32)
     // Create distance matrix. Even if I do this initial check for now I conside x and y of the
     // same dimension.
     // TODO implement version with x and y of different length
-    let distance_matrix = if x.len() >= y.len() {
+    let mut distance_matrix = if x.len() >= y.len() {
         Array::<f32, _>::zeros((x.len(), y.len()).f())
     } else {
         Array::<f32, _>::zeros((y.len(), x.len()).f())
     };
+    
+    // Compute element in position (0, 0)
+    distance_matrix[[0, 0]] = l_norm(x[0], y[0], 2);
+
     println!("{:?}", distance_matrix);
 
     let n_diagonals = x.len() + y.len() - 1;
@@ -34,40 +46,74 @@ pub fn compute_1d(x : ArrayViewD<'_, f32>, y : ArrayViewD<'_, f32>, gamma : f32)
     // The firs diagonal has only 1 element, i.e. the element in posizion (0, 0)
     // The second diagonal has 2 elements, i.e. the elements in position (0, 1) and (1, 0) 
     let mut n_elements_diagonal = 1;
-    for n_1 in 0..n_diagonals{ // Iterate through the diagonals
-        println!("\t{n_1}, {n_elements_diagonal}");
+
+    // Compute the shift (used to compute the correct indices if you are in the lower right
+    // section of the matrix)
+    let mut shift = 0;
+    
+    println!("n_diagonals = {}, n_diagonals / 2 = {}", n_diagonals, n_diagonals / 2);
+    // Compute soft-dtw for each element of the matrix
+    for n_1 in 1..n_diagonals{ // Iterate through the diagonals
         // Compute the number of element in the diagonals
         // TODO modify when add arrays of different length
+        let computation_in_the_upper_left_section : bool;
         if n_1 <= n_diagonals / 2 { // Upper left of the distance matrix
             n_elements_diagonal += 1;
+            computation_in_the_upper_left_section = true;
         } else { // Lower right of the distance matrix
             n_elements_diagonal -= 1;
-
+            computation_in_the_upper_left_section = false;
         }
+        
+        // Compute the shift for the lower right section of the matrix
+        shift = if computation_in_the_upper_left_section {
+            0
+        } else {
+            shift + 1
+        };
+        
+        // Print for debug
+        // println!("n_1 = {} ({}), n_elements_diagonal = {}, upper_left_section = {}, shift = {}", n_1, n_1 + 1, n_elements_diagonal, computation_in_the_upper_left_section, shift);
 
         // Indices
         // let idx_array = Array::<f32, _>::range(0., n_elements_diagonal as f32, 1.);
         // idx 1 is used for the row of the matrix
         // idx 2 is used for the column of the matrix
-        for idx_1 in 0..n_elements_diagonal {
-            let idx_2 = n_elements_diagonal - 1 - idx_1;
+        for n_2 in 0..n_elements_diagonal {
+            // Set indices
+            let idx_1 = n_2 + shift;
+            let idx_2 = n_elements_diagonal - 1 - n_2 + shift;
+
+            // Print for debug
+            // println!("\tidx_1 = {}, idx_2 = {}", idx_1, idx_2);
+
+            // Get the value above the one I want to compute
+            let left_value = if idx_1 - 1 < 0 {
+                f32::MAX
+            } else {
+                distance_matrix[[idx_1 - 1, idx_2]]
+            };
             
             // Get the value above the one I want to compute
             let upper_value = if idx_2 - 1 < 0 {
                 f32::MAX
             } else {
-                1.
+                distance_matrix[[idx_1, idx_2 - 1]]
             };
+
+            // Get the value above and on the left (i.e. in diagonal) respect the one I want to compute
+            let upper_left_value = if idx_1 - 1 < 0 || idx_2 -1 < 0 {
+                f32::MAX
+            } else {
+                distance_matrix[[idx_1 - 1, idx_2 - 1]]
+            };
+            
+            // Compute the dtw value for the current cell
+            distance_matrix[[idx_1, idx_2]] = l_norm(x[idx_1], y[idx_2], 2) + compute_soft_min_optimized(upper_value, upper_left_value, left_value, gamma);
         }
     }
 
-    return result
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-fn compute_distance_matrix_value() -> f32 {
-    return 1.
+    return distance_matrix 
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
